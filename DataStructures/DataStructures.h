b74faebb4b5e0b756c485d7f9f7b4932692dae3b
Ricardo Maqueda <ricardo.maqueda@tui.co.uk>
2020-02-27 09:11:22 +0000

    Add xcode project

---
+++ DataStructures/DataStructures.h
@@ -0,0 +1,19 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Ricardo.Maqueda on 27/02/2020.
+//  Copyright © 2020 Ricardo.Maqueda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
